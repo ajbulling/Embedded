1a3bd95fc461e11ee47233913d9aa31af24809b2
Jesse Tutor <jessetutor97@gmail.com>
2020-02-05 15:21:18 -0600

    Adding scons build support for lab 3

--- pic24lib_all/SCons_build.py
+++ pic24lib_all/SCons_build.py
@@ -164,3 +164,5 @@ if 'explorer' in buildTargets:
   buildWithCommonSources(['explorer16_100p/*.c'], PIC24SupportLibObjects, env, {}, 'explorer')
 if 'lab2' in buildTargets:
   buildWithCommonSources(['lab2/*.c'], PIC24SupportLibObjects, env, {}, 'lab2')
+if 'lab3' in buildTargets:
+  buildWithCommonSources(['lab3/*.c'], PIC24SupportLibObjects, env, {}, 'lab3')
