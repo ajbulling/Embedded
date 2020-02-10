6521a1cee92cd203f91532b24e608e8c216a392a
Andrew Bullington <ajb946@msstate.edu>
2020-02-10 14:25:19 -0600

    Added untested functionality for LED3 and LED2 in t3_app.c

--- lab3/t3_app.c
+++ lab3/t3_app.c
@@ -5,5 +5,39 @@
 #include "revF14.h"
 #include "pic24_all.h"
 
+ESOS_USER_TASK( LED3_blink ){
+    while ( TRUE ){
+        esos_uiF14_flashLED3(500);
+    }
+}
 
-void user_init() {}
+ESOS_USER_TASK( LED2_state ){
+    while ( TRUE ){
+        if ( esos_uiF14_getRpgVelocity_i16() == 0){
+	    esos_uiF14_turnLED2Off();
+	}
+        else if ( esos_uiF14_isRpgTurningSlow() ){
+	    esos_uiF14_turnLED2On();
+	}
+        else if ( esos_uiF14_isRpgTurningMedium() ){
+	    while ( esos_uiF14_isRpgTurningMedium() ){
+	        esos_uiF14_flashLED2(500);
+            }
+	}
+	else if ( esos_uiF14_isRpgTurningFast() ){
+	    while ( esos_uiF14_isRpgTurningFast() ){
+                esos_uiF14_flashLED2(100);
+	    }
+        }
+    }
+}
+
+void user_init() {
+    // Configgure LED hardware
+    LED1_CONFIG();
+    LED2_CONFIG();
+    LED3_HB_CONFIG();
+
+    // Start ESOS tasks
+    esos_RegisterTask( LED3_blink );
+}
