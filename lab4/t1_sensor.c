ac0f6c9fb41b7f4519360f96d2c13f59afa6406b
Andrew Bullington <ajb946@msstate.edu>
2020-02-22 16:14:53 -0600

    Added heartbeat

--- lab4/t1_sensor.c
+++ lab4/t1_sensor.c
@@ -14,10 +14,20 @@
 
 uint16_t pot_data;
 
+// ESOS task for Heartbeat LED3
+ESOS_USER_TASK ( LED3_blink ) {
+    ESOS_TASK_BEGIN();
+    while ( TRUE ){
+        esos_uiF14_toggleLED3();
+        ESOS_TASK_WAIT_TICKS(250);
+    }
+    ESOS_TASK_END();
+}
+
 ESOS_USER_TASK ( QUICK_READ_TEST ) {
     ESOS_TASK_BEGIN();
     while ( TRUE ){
-        // Ch 2
+        // Potentiometer is on channel 2
         ESOS_TASK_WAIT_ON_AVAILABLE_SENSOR(ESOS_SENSOR_CH02, ESOS_SENSOR_VREF_1V0);
         ESOS_TASK_WAIT_SENSOR_QUICK_READ(pot_data);
         ESOS_SENSOR_CLOSE();
@@ -26,14 +36,24 @@ ESOS_USER_TASK ( QUICK_READ_TEST ) {
         ESOS_TASK_WAIT_ON_SEND_UINT32_AS_HEX_STRING((uint32_t)pot_data);
         ESOS_TASK_WAIT_ON_SEND_UINT8('\n');
         ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
-        
+
+        ESOS_TASK_YIELD();        
     }
     ESOS_TASK_END();
 }
 
 void user_init() {
     CONFIG_POT();
+    
+    LED1_CONFIG();
+    LED2_CONFIG();
+    LED3_HB_CONFIG();
 
+    SW1_CONFIG();
+    SW2_CONFIG();
+    SW3_CONFIG();
+
+    esos_RegisterTask( LED3_blink );
     esos_RegisterTask( QUICK_READ_TEST );
 }
 
