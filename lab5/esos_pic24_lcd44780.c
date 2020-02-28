d105887d5eb83774c657c3012a34194c3873f153
Will Gaines <twg79@msstate.edu>
2020-02-27 18:33:02 -0600

    RW pin set

--- lab5/esos_pic24_lcd44780.c
+++ lab5/esos_pic24_lcd44780.c
@@ -77,10 +77,12 @@ void __esos_lcd44780_pic24_configDataPinsAsInput( void ) {
 	// write the hardware-specific code to set the LCD character module
 	// data pins to be "inputs" to prepare for a read of the LCD module
     CONFIG_LCD_INPUT();
+	__ESOS_LCD44780_HW_SET_RW_READ()
 }
 
 void __esos_lcd44780_pic24_configDataPinsAsOutput( void ) {
 	// write the hardware-specific code to set the LCD character module
 	// data pins to be "outputs" to prepare for a write to the LCD module
     CONFIG_LCD_OUTPUT();
+	__ESOS_LCD44780_HW_SET_RW_WRITE();
 }
